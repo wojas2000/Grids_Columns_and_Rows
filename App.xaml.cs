00dde51216b62ff800a983aa41e624bd0e6d0c0e
wojas2000 <wojas2000@o2.pl>
2022-01-28 13:20:25 +0100

    Changes with add Thread.Sleep and SplashScreen.

--- App.xaml.cs
+++ App.xaml.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Threading;
 
 namespace Grids_Columns_and_Rows
 {
@@ -13,5 +14,11 @@ namespace Grids_Columns_and_Rows
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            Thread.Sleep(3000);
+        }
     }
 }
